50f647cf29b58462fcdd044419f89e07bfeaeb3d
Lauge Møller Andersen <46719124+LaugeM@users.noreply.github.com>
2026-03-15 12:55:29 +0100

    Add project files.

---
+++ BF6PowerSaver/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace BF6PowerSaver
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
